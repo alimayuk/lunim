bf7de1ce38ae8fdee2c98007a1f598f860ba20dc
puply <alimayuk22@hotmail.com>
2023-07-30 00:27:01 +0300

    fix: typo

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Footer from './Layout/Footer/Footer';
 import Home from "./components/Home"
 import About from './components/About';
 import Services from './components/Services';
-import Projects from './components/projects';
+import Projects from './components/Projects';
 import Testimonials from './components/Testimonials';
 
 
